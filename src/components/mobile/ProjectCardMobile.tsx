92aa475375fad52c3d9359022862b1b21198b8c1
Mahmoud Mohamed <mahmoud@null.net>
2024-06-28 02:29:34 -0400

    Updated HeroMobile top margin and fixed ProjectCardMobile styles

--- src/components/mobile/ProjectCardMobile.tsx
+++ src/components/mobile/ProjectCardMobile.tsx
@@ -37,99 +37,18 @@ const ProjectCardMobile = (props: {
   video: string | undefined;
   image: string | undefined;
 }) => {
-  // const [hover, setHover] = useState(false);
-  //
-  // const ref = useRef<HTMLDivElement | null>(null);
-  //
-  // const x = useMotionValue(0);
-  // const y = useMotionValue(0);
-  //
-  // const xSpring = useSpring(x);
-  // const ySpring = useSpring(y);
-  //
-  // const transform = useMotionTemplate`rotateX(${xSpring}deg) rotateY(${ySpring}deg)`;
-  //
-  // const handleMouseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-  //   setHover(true);
-  //
-  //   if (!ref.current) return [0, 0];
-  //
-  //   const rect = ref.current.getBoundingClientRect();
-  //
-  //   const width = rect.width;
-  //   const height = rect.height;
-  //
-  //   const mouseX = (e.clientX - rect.left) * ROTATION_RANGE;
-  //   const mouseY = (e.clientY - rect.top) * ROTATION_RANGE;
-  //
-  //   const rX = (mouseY / height - HALF_ROTATION_RANGE) * -1;
-  //   const rY = mouseX / width - HALF_ROTATION_RANGE;
-  //
-  //   x.set(rX);
-  //   y.set(rY);
-  // };
-  //
-  // const handleMouseLeave = () => {
-  //   setHover(false);
-  //   x.set(0);
-  //   y.set(0);
-  // };
-
   const handleClick = () => {
     window.open(props.link);
   };
 
   return (
-    <div
-      // ref={ref}
-      // onMouseMove={handleMouseMove}
-      // onMouseLeave={handleMouseLeave}
-      onClick={handleClick}
-      // style={{
-      //   transformStyle: "preserve-3d",
-      //   transform,
-      //   cursor: "alias",
-      // }}
-      className="MotionDiv"
-    >
-      <div
-        className="ProjectCard"
-        // style={{
-        //   transform: "translateZ(75px)",
-        //   transformStyle: "preserve-3d",
-        // }}
-      >
-        <div
-          className="CardImg"
-          // style={{
-          //   transform: "translateZ(75px)",
-          // }}
-        >
-          {/*{hover ? (*/}
-          {/*  <video*/}
-          {/*    className="ProjectImg"*/}
-          {/*    src={props.video}*/}
-          {/*    loop*/}
-          {/*    autoPlay*/}
-          {/*    preload=""*/}
-          {/*    playsInline={true}*/}
-          {/*  />*/}
-          {/*) : (*/}
+    <div onClick={handleClick} className="MotionDiv">
+      <div className="ProjectCard">
+        <div className="CardImg">
           <img className="ProjectImg" src={props.image} alt="Project Image" />
-          {/*)}*/}
         </div>
-        <div
-          className="ProjectDetails"
-          // style={{
-          //   transform: "translateZ(75px)",
-          // }}
-        >
-          <div
-            className="ProjectInfo"
-            // style={{
-            //   transform: "translateZ(75px)",
-            // }}
-          >
+        <div className="ProjectDetails">
+          <div className="ProjectInfo">
             <h1 className="ProjectName">{props.name}</h1>
             <p className="ProjectDescription">{props.description}</p>
           </div>
